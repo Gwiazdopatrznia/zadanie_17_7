439e1e9a2edf55c879c26c5993030599481802f8
Anna Gajewska <gajewska.anna@o2.pl>
2018-06-18 03:33:55 +0200

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '561014164250-334n6uh7flobln2o6dbv1lncsurlkcar.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'zZ4cYiK4Z0OhKrGh6PHtYJn4',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
